55411bb78b184dd0412b6940a56f0f0e89808a78
zhaowenhao <zwh877719756@163.com>
2022-07-27 17:47:29 +0800

    Update Bomb.h

--- Bomb.h
+++ Bomb.h
@@ -20,7 +20,7 @@ public:
     void run() {
         while(isLive) {
             //
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
             //
             lifeDown();
         }
