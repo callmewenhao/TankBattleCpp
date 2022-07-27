79b90c6ce7fa3fd23df7012f8950726f45a6bc8b
zhaowenhao <zwh877719756@163.com>
2022-07-27 17:48:27 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,7 +4,6 @@
 
 #include "Game.h"
 
-
 void Game::drawBombs() {
     for (auto bombIter = bombs.begin(); bombIter != bombs.end(); ) {
         if ((*bombIter)->life > 6) {
@@ -23,6 +22,22 @@ void Game::drawBombs() {
         if (!(*bombIter)->isLive) {
             bombIter = bombs.erase(bombIter);
         }
+    }
+}
+
+void Game::hitTank(std::shared_ptr<Bullet> bullet, std::shared_ptr<Tank> tank) {
+
+    if(tank->x < bullet->x && bullet->x < tank->x + 40
+       && tank->y < bullet->y && bullet->y < tank->y + 40) {
+        bullet->isLive = false;
+        tank->isLive = false;
+
+        // build a bomb
+        auto bomb = std::make_shared<Bomb>(tank->x, tank->y);
+        bombs.push_back(bomb);
+        // thread for bomb
+        std::thread t(&Bomb::run, bomb);
+        t.detach();
     }
 }
 
@@ -145,16 +160,14 @@ void Game::drawHero() {
     }
 }
 
-void Game::play() {
+void Game::update() {
 
     ExMessage exMessage;
-
-    while (true) {
+    while (isLive) {
         if (peekmessage(&exMessage, EM_KEY, true)) {
-            // new position
+            // get message from message queue
             if (exMessage.message == WM_KEYDOWN) {
                 std::shared_ptr<Bullet> bullet = nullptr;
-
                 switch (exMessage.vkcode) {
                     case VK_UP:
                         std::cout << "up!" << std::endl;
@@ -191,45 +204,115 @@ void Game::play() {
             }
         }
 
-        //
+        // hero's bullets hit enemies
         hitEnemy();
 
+        // detect enemy tank crash
+        crashEnemy();
+
+        // sleep 100 ms
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    }
+}
+
+void Game::paint() {
+
+    while (isLive) {
+        if(this->enemyArr.empty()) {
+            HWND hWnd = GetHWnd();//获取窗口句柄
+            int choose = MessageBox(hWnd, "continue? please choose", "hello bro!" ,MB_OKCANCEL);//设置模态对话框
+            if(choose == IDOK) {
+                initTanks();
+            } else {
+                isLive = false; // thread is over!
+            }
+        }
+
+        // batch paint the images
+        BeginBatchDraw();
         // clear panel
         cleardevice();
-        // paint hero
-        drawHero();
+        // paint enemies bullets
+        drawEnemyBullets();
         // paint hero bullets
         drawHeroBullets();
         // paint enemies
         drawEnemies();
-        // paint enemies bullets
-        drawEnemyBullets();
-        //
+        // paint hero
+        drawHero();
+        // paint bombs
         drawBombs();
+        // display the images
+        EndBatchDraw();
 
         // sleep 100 ms
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
 
+}
+
+Game::Game() {
+    // init Enemies
+    initTanks();
+    // load the images
+    loadImages();
+}
+
+void Game::crashEnemy() {
+
+    for (auto enemy : enemyArr) {
+        if(enemy->isLive && hero->isOverlap(enemy)) {
+            enemy->isLive = false;
+            // build a bomb
+            auto bomb = std::make_shared<Bomb>(enemy->x, enemy->y);
+            bombs.push_back(bomb);
+            // thread for bomb
+            std::thread t(&Bomb::run, bomb);
+            t.detach();
+        }
     }
 }
 
-void Game::hitTank(std::shared_ptr<Bullet> bullet, std::shared_ptr<Tank> tank) {
+void Game::initTanks() {
+    // init hero
+    hero = std::make_shared<Hero>(450, 450, 0);
 
-    if(tank->x < bullet->x && bullet->x < tank->x + 40
-        && tank->y < bullet->y && bullet->y < tank->y + 40) {
-        bullet->isLive = false;
-        tank->isLive = false;
-
-        // build a bomb
-        auto bomb = std::make_shared<Bomb>(tank->x, tank->y);
-        bombs.push_back(bomb);
-        // thread for bomb
-        std::thread t(&Bomb::run, bomb);
+    // init enemies
+    for (int i = 0; i < enemyNum; ++i) {
+        auto e = std::make_shared<Enemy>(100 * (i + 1), 200, i % 4, this->enemyArr);
+        e->speed = 1;
+        enemyArr.push_back(e);
+        std::thread t(&Enemy::run, e);
         t.detach();
     }
-
 }
 
+void Game::loadImages() {
+    // load the hero images
+    loadimage(&tankUp, _T("resources/hero/hero1U.gif"));
+    loadimage(&tankRight, _T("resources/hero/hero1R.gif"));
+    loadimage(&tankDown, _T("resources/hero/hero1D.gif"));
+    loadimage(&tankLeft, _T("resources/hero/hero1L.gif"));
+    // load the enemy images
+    loadimage(&enemyUp, _T("resources/enemy/enemy1U.gif"));
+    loadimage(&enemyRight, _T("resources/enemy/enemy1R.gif"));
+    loadimage(&enemyDown, _T("resources/enemy/enemy1D.gif"));
+    loadimage(&enemyLeft, _T("resources/enemy/enemy1L.gif"));
+    // load the bullet images
+    loadimage(&bulletUp, _T("resources/bullet/bullet_up.png"));
+    Resize(&bulletUp, 7, 7);
+    loadimage(&bulletRight, _T("resources/bullet/bullet_right.png"));
+    Resize(&bulletRight, 7, 7);
+    loadimage(&bulletDown, _T("resources/bullet/bullet_down.png"));
+    Resize(&bulletDown, 7, 7);
+    loadimage(&bulletLeft, _T("resources/bullet/bullet_left.png"));
+    Resize(&bulletLeft, 7, 7);
+    // load bombs images
+    loadimage(&bomb1, _T("resources/boom/blast8.gif"));
+    loadimage(&bomb2, _T("resources/boom/blast6.gif"));
+    loadimage(&bomb3, _T("resources/boom/blast4.gif"));
+}
 
 
 
