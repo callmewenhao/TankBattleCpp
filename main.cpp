6fcaa61c9ceddd2db7409a0bbdf071e83440efed
zhaowenhao <zwh877719756@163.com>
2022-07-28 16:57:20 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 #include <conio.h>
 #include <graphics.h>
 #include "Game.h"
+#include <mmsystem.h>
+#pragma comment(lib,"winmm.lib")
 
 int main() {
 
-    initgraph(1000, 700);    // init the windows
-
+    // play the music
+    PlaySound(TEXT("resources/audios/start.wav"), nullptr, SND_FILENAME | SND_ASYNC | SND_LOOP );
+    //
+    initgraph(1000, 700);  // init the windows
     // our main thread
     Game game;
     std::thread repaint(&Game::paint, &game); // a ref or ptr for second para
     std::thread updateInfo(&Game::update, &game); // a ref or ptr for second para
     repaint.join();
     updateInfo.join();
-
-    closegraph();            // 关闭绘图窗口
+    //
+    closegraph();
     return 0;
 
 }
